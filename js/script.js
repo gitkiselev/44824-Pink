1ae277c8c7cd7c6d89a4013ade8ed47f70bc5e41
gitkiselev <482003@mail.ru>
2015-04-05 17:57:24 +0300

    Что успел

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-var open = document.querySelector(".js-open")
+var link = document.querySelector(".page-header__open");
+var close = document.querySelector(".page-header__close");
+var opened = document.querySelector(".main-nav");
+var bg = document.querySelector(".page-header__opacity");
+
+
+link.addEventListener("click", function(event) {
+event.preventDefault();
+opened.classList.toggle("main-nav-js");//выскакивает/убирается меню
+link.classList.toggle("page-header__close");//бургер меняется на крестикadd
+//link.classList.remove("page-header__open");//при щелчке удаляется бургер
+bg.classList.toggle("page-header__opacity--no");//делаем фон полностью непрозрачным
+
+
+
+
+});
+
+
+
+
+
+
+
+
